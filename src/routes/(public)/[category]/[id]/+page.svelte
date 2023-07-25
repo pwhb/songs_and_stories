c079b04161e965ffa58ed95f7a0d60e4995c022e
pwhb <pwhbdev@gmail.com>
2023-07-25 11:09:42 +0630

    better editor

--- src/routes/(public)/[category]/[id]/+page.svelte
+++ src/routes/(public)/[category]/[id]/+page.svelte
@@ -19,7 +19,7 @@
 
 <Meta {metadata} />
 <div class="mx-auto max-w-4xl">
-	<section class="p-10 md:text-xl">
+	<section class="p-2 md:p-10 md:text-xl">
 		<div class="mb-10 text-center">
 			<h1 class="mb-10 text-2xl font-bold text-primary">{title}</h1>
 			<div class="flex flex-row gap-3 justify-center">
@@ -49,7 +49,8 @@
 				/>
 			</div>
 		{/if}
-		<div class="divider" />u
+		<div class="divider" />
+
 		{@html marked(body).replace(/\n/g, '<br>')}
 		<div class="divider" />
 		<NavButtons />
